b84e06b8c103e68f2f765d223f7cb4bb4908a16e
Mirko Acimovic <acim.mirko@gmail.com>
2021-11-15 19:32:57 +0100

    Refactored console app according to Autofac DI setup

--- TheShop/Application.cs
+++ TheShop/Application.cs
@@ -8,9 +8,48 @@ namespace TheShop
 {
     public class Application
     {
+        public Application()
+        {
+			// TODO: Inject all services needed
+        }
+
         public void Run(string[] args)
         {
-            Console.ReadKey();
-        }
-    }
+			//var shopService = new ShopService();
+
+			//try
+			//{
+			//	//order and sell
+			//	shopService.OrderAndSellArticle(1, 20, 10);
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine(ex);
+			//}
+
+			//try
+			//{
+			//	//print article on console
+			//	var article = shopService.GetById(1);
+			//	Console.WriteLine("Found article with ID: " + article.ID);
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine("Article not found: " + ex);
+			//}
+
+			//try
+			//{
+			//	//print article on console				
+			//	var article = shopService.GetById(12);
+			//	Console.WriteLine("Found article with ID: " + article.ID);
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine("Article not found: " + ex);
+			//}
+
+			//Console.ReadKey();
+		}
+	}
 }
